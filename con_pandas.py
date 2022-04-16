a4abc052a271b9f8d7cdb2d71bedfad100c3872b
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-16 20:51:48 +0200

    enunciado añadido

--- con_pandas.py
+++ con_pandas.py
@@ -4,9 +4,23 @@ import pandas as pd
 df = pd.read_csv('vehicles(20k).csv')
 hf= df.head(10)
 
+"""
+-Crear, leer y escribir(No puede trabajar con datos si no puede leerlos)
+-Indexación, selección y asignación(Los científicos de datos profesionales
+hacen esto docenas de veces al día)
+-Resumen de funciones y mapas(Extraiga conocimientos de sus datos)
+-Agrupación y clasificación(Aumente su nivel de conocimiento. Cuanto
+ más complejo es el conjunto de datos, más importa esto)
+-Tipos de datos y valores perdidos(Lidiar con los problemas
+ de bloqueo de progreso más comunes)
+-Renombrar y combinar(Los datos provienen de muchas fuentes.
+ Ayude a que todo tenga sentido juntos)
 
-clomnas=[]
-lista=[]
+"""
+
+
+
+#Lectura y escritura
 
 print("==================================================")
 print("           Informacion. Sobre el CSV")
