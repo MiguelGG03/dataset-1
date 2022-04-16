59b912eadab80e6d7725e34ffaddc2b344b281e1
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-16 20:49:50 +0200

    cambios tontos

--- con_pandas.py
+++ con_pandas.py
@@ -8,16 +8,16 @@ hf= df.head(10)
 clomnas=[]
 lista=[]
 
-
 print("==================================================")
 print("           Informacion. Sobre el CSV")
 print("==================================================")
 print()
 print(df.info())
-
+print()
 
 print("==================================================")
 print("      Primeras 10 lineas del fichero CSV")
 print("==================================================")
 print()
 print(hf)
+print()
